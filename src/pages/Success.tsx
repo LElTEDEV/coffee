e9f66e2b2f6ca363cec59ee2b1e494451720ac01
LElTEDEV <zinenizi@gmail.com>
2025-12-05 16:35:54 -0300

    feat: created success page

---
+++ src/pages/Success.tsx
@@ -0,0 +1,43 @@
+import { Clock, DollarSign, MapPin } from "lucide-react";
+import illustrationSvg from "../assets/illustration.svg";
+import { SuccessInfo } from "../components/SuccessInfo";
+
+export function Success() {
+  return (
+    <div className="mt-6 xl:mt-20 flex flex-col md:flex-row items-center gap-20">
+      <main>
+        <h1 className="font-baloo text-baloo-m xl:text-baloo-l text-yellow-dark font-extrabold leading-130">
+          Uhu! Pedido confirmado
+        </h1>
+
+        <p className="text-m xl:text-l text-base-subtitle">
+          Agora é só aguardar que logo o café chegará até você
+        </p>
+
+        <section className="lg:w-[526px] border p-10 rounded rounded-tr-4xl rounded-bl-4xl border-yellow mt-5 xl:mt-10 flex flex-col gap-8">
+          <SuccessInfo icon={MapPin} className="bg-purple">
+            <p className="text-base-text text-xs lg:text-m">
+              Entrega em Rua João Daniel Martinelli, 102 Farrapos - Porto
+              Alegre, RS
+            </p>
+          </SuccessInfo>
+
+          <SuccessInfo icon={Clock} className="bg-yellow">
+            <p className="text-base-text text-xs lg:text-m">
+              Previsão de entrega <p className="font-bold">20 min - 30 min</p>
+            </p>
+          </SuccessInfo>
+
+          <SuccessInfo icon={DollarSign} className="bg-yellow-dark">
+            <p className="text-base-text text-xs lg:text-m">
+              Pagamento na entrega{" "}
+              <p className="font-bold">Cartão de Crédito</p>
+            </p>
+          </SuccessInfo>
+        </section>
+      </main>
+
+      <img src={illustrationSvg} alt="" className="w-3xs xl:w-3xl" />
+    </div>
+  );
+}
