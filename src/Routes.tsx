942b334312aae69165ead3d00f2d1b13baaa9a6d
LElTEDEV <zinenizi@gmail.com>
2025-12-05 13:49:10 -0300

    feat: created app routes

---
+++ src/Routes.tsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import { DefaultLayout } from "./layouts/DefaultLayout";
+
+import { Home } from "./pages/Home";
+
+export function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<DefaultLayout />}>
+        <Route index element={<Home />} />
+      </Route>
+    </Routes>
+  );
+}
