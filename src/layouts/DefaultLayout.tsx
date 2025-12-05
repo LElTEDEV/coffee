89aa9df5e9a0584eaf140d07dbeb9fa65463ec58
LElTEDEV <zinenizi@gmail.com>
2025-12-05 10:40:16 -0300

    feat: add max width

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -6,28 +6,30 @@ import { MapPin, ShoppingCart } from "lucide-react";
 export function DefaultLayout() {
   return (
     <div className="w-screen px-4 md:px-20 lg:px-40">
-      <div className="flex items-center justify-between py-8">
-        <img src={logoSvg} alt="" />
+      <div className="max-w-6xl mx-auto">
+        <div className="flex items-center justify-between py-8">
+          <img src={logoSvg} alt="" />
 
-        <nav className="flex items-center gap-3">
-          <span className="flex items-center gap-1 p-2 rounded bg-purple-light text-purple-dark">
-            <MapPin size={22} />
-            São Paulo, SP
-          </span>
+          <nav className="flex items-center gap-3">
+            <span className="flex items-center gap-1 p-2 rounded bg-purple-light text-purple-dark">
+              <MapPin size={22} />
+              São Paulo, SP
+            </span>
 
-          <NavLink
-            to="/check-out"
-            className="group bg-yellow-light p-2 rounded hover:bg-yellow-dark transition-colors ease-in"
-          >
-            <ShoppingCart
-              size={22}
-              className="text-yellow-dark group-hover:text-white transition-colors ease-in"
-            />
-          </NavLink>
-        </nav>
+            <NavLink
+              to="/check-out"
+              className="group bg-yellow-light p-2 rounded hover:bg-yellow-dark transition-colors ease-in"
+            >
+              <ShoppingCart
+                size={22}
+                className="text-yellow-dark group-hover:text-white transition-colors ease-in"
+              />
+            </NavLink>
+          </nav>
+        </div>
+
+        <Outlet />
       </div>
-
-      <Outlet />
     </div>
   );
 }
