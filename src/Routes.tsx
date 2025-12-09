e611aeea7fd73a25efa64871b064ded39a89edb7
LElTEDEV <zinenizi@gmail.com>
2025-12-09 10:24:03 -0300

    feat: adicionado rota para pagina de not found

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -3,6 +3,8 @@ import { DefaultLayout } from "./layouts/DefaultLayout";
 
 import { Home } from "./pages/Home";
 import { Success } from "./pages/Success";
+import { CheckOut } from "./pages/Checkout";
+import { NotFound } from "./pages/NotFound";
 
 export function AppRoutes() {
   return (
@@ -10,7 +12,10 @@ export function AppRoutes() {
       <Route path="/" element={<DefaultLayout />}>
         <Route index element={<Home />} />
         <Route path="/success" element={<Success />} />
+        <Route path="/check-out" element={<CheckOut />} />
       </Route>
+
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
